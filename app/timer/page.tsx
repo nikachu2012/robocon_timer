336f13af9b34437a42d38ea1f8d1eb860adc641b
nikachu2012 <webmaster@nikachu.net>
2024-12-13 20:06:32 +0900

    [add] 音声再生を追加とカウントダウンからカウントアップに変更

--- app/timer/page.tsx
+++ app/timer/page.tsx
@@ -10,33 +10,46 @@ export default function TimerPage() {
     const searchParams = useSearchParams();
     const time = parseInt(searchParams.get("t") || "0") || 0;
 
-    const [timer, setTimer] = useState<number>(time);
+    const [timer, setTimer] = useState<number>(-6);
     const timerRef = useRef(timer)
+
+    const playerRef = useRef(null);
 
     useEffect(() => {
         timerRef.current = timer;
     }, [timer]);
 
     useEffect(() => {
-        setTimer(time);
 
         const intervalID = setInterval(() => {
-            if (timerRef.current > 0) {
-                setTimer(timer => timer - 1);
+            console.log("setinterval called")
+            if ((time - timerRef.current) > 0) {
+                setTimer(timer => timer + 1);
             }
             else {
                 clearInterval(intervalID);
             }
+
+            if ((time - timerRef.current) == 4) {
+                console.log("play")
+
+                // if (playerRef.current != null)
+                playerRef.current.play();
+            }
+
             console.log("looppp")
         }, 1000);
-
         return () => clearInterval(intervalID)
     }, []);
 
 
     return <>
         <div className="w-[100vw] h-[100vh] flex items-center justify-center flex-col gap-3">
-            <div className="text-9xl font-bold">{timer}</div>
+            <div className="text-9xl font-bold">{timer == -6 ? "READY" : (timer == 0 || timer == 1) ? "START" : Math.abs(timer)}</div>
+            <audio
+                src="/jihou.mp3"
+                ref={playerRef}
+            />
 
             <Link href={"/"}>
                 <Button><Undo2 />戻る</Button>
